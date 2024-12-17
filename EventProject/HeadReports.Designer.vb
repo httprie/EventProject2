8898d50b59f41340b1cd4eac98c15436201de7f8
httprie <basilio.riezseht.01289@dyci.edu.ph>
2024-12-17 12:02:02 +0800

    reports + attendancelog

--- EventProject/HeadReports.Designer.vb
+++ EventProject/HeadReports.Designer.vb
@@ -29,17 +29,18 @@ Partial Class HeadReports
         Me.timedate = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
-        Me.cbFilter = New System.Windows.Forms.ComboBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.btnSearchRep = New Guna.UI2.WinForms.Guna2Button()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
-        Me.cbData = New System.Windows.Forms.ComboBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ReportEnd = New Guna.UI2.WinForms.Guna2DateTimePicker()
         Me.ReportStart = New Guna.UI2.WinForms.Guna2DateTimePicker()
         Me.ReportData = New Guna.UI2.WinForms.Guna2DataGridView()
         Me.btnPrint = New Guna.UI2.WinForms.Guna2Button()
+        Me.cbFilter = New System.Windows.Forms.ComboBox()
+        Me.cbData = New System.Windows.Forms.ComboBox()
+        Me.btnAttendance = New Guna.UI2.WinForms.Guna2Button()
         CType(Me.ReportData, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -69,17 +70,6 @@ Partial Class HeadReports
         'Timer1
         '
         '
-        'cbFilter
-        '
-        Me.cbFilter.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cbFilter.ForeColor = System.Drawing.Color.Black
-        Me.cbFilter.FormattingEnabled = True
-        Me.cbFilter.Items.AddRange(New Object() {"STUDENT ID", "FIRST NAME", "LAST NAME", "VENUE", "DEPARTMENT", "FACILITATOR", "EVENT ID", "COURSE", "YEAR", "SECTION", ""})
-        Me.cbFilter.Location = New System.Drawing.Point(19, 90)
-        Me.cbFilter.Name = "cbFilter"
-        Me.cbFilter.Size = New System.Drawing.Size(343, 33)
-        Me.cbFilter.TabIndex = 44
-        '
         'Label4
         '
         Me.Label4.AutoSize = True
@@ -100,9 +90,9 @@ Partial Class HeadReports
         Me.btnSearchRep.FillColor = System.Drawing.Color.White
         Me.btnSearchRep.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold)
         Me.btnSearchRep.ForeColor = System.Drawing.Color.Black
-        Me.btnSearchRep.Location = New System.Drawing.Point(832, 90)
+        Me.btnSearchRep.Location = New System.Drawing.Point(718, 90)
         Me.btnSearchRep.Name = "btnSearchRep"
-        Me.btnSearchRep.Size = New System.Drawing.Size(157, 35)
+        Me.btnSearchRep.Size = New System.Drawing.Size(123, 28)
         Me.btnSearchRep.TabIndex = 54
         Me.btnSearchRep.Text = "Search"
         '
@@ -127,17 +117,6 @@ Partial Class HeadReports
         Me.Label9.Size = New System.Drawing.Size(116, 22)
         Me.Label9.TabIndex = 56
         Me.Label9.Text = "END DATE:"
-        '
-        'cbData
-        '
-        Me.cbData.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cbData.ForeColor = System.Drawing.Color.Black
-        Me.cbData.FormattingEnabled = True
-        Me.cbData.Items.AddRange(New Object() {"STUDENT ID", "FIRST NAME", "LAST NAME", "VENUE", "DEPARTMENT", "FACILITATOR", "EVENT ID", "COURSE", "YEAR", "SECTION", ""})
-        Me.cbData.Location = New System.Drawing.Point(390, 90)
-        Me.cbData.Name = "cbData"
-        Me.cbData.Size = New System.Drawing.Size(426, 33)
-        Me.cbData.TabIndex = 57
         '
         'Label1
         '
@@ -255,22 +234,56 @@ Partial Class HeadReports
         Me.btnPrint.TabIndex = 63
         Me.btnPrint.Text = "Print"
         '
+        'cbFilter
+        '
+        Me.cbFilter.Font = New System.Drawing.Font("Ubuntu", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbFilter.FormattingEnabled = True
+        Me.cbFilter.Location = New System.Drawing.Point(18, 90)
+        Me.cbFilter.Name = "cbFilter"
+        Me.cbFilter.Size = New System.Drawing.Size(240, 28)
+        Me.cbFilter.TabIndex = 64
+        '
+        'cbData
+        '
+        Me.cbData.Font = New System.Drawing.Font("Ubuntu", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbData.FormattingEnabled = True
+        Me.cbData.Location = New System.Drawing.Point(264, 90)
+        Me.cbData.Name = "cbData"
+        Me.cbData.Size = New System.Drawing.Size(448, 28)
+        Me.cbData.TabIndex = 65
+        '
+        'btnAttendance
+        '
+        Me.btnAttendance.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnAttendance.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnAttendance.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnAttendance.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnAttendance.FillColor = System.Drawing.Color.White
+        Me.btnAttendance.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.btnAttendance.ForeColor = System.Drawing.Color.Black
+        Me.btnAttendance.Location = New System.Drawing.Point(847, 90)
+        Me.btnAttendance.Name = "btnAttendance"
+        Me.btnAttendance.Size = New System.Drawing.Size(142, 28)
+        Me.btnAttendance.TabIndex = 66
+        Me.btnAttendance.Text = "Attendance"
+        '
         'HeadReports
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
         Me.ClientSize = New System.Drawing.Size(1011, 800)
         Me.ControlBox = False
+        Me.Controls.Add(Me.btnAttendance)
+        Me.Controls.Add(Me.cbData)
+        Me.Controls.Add(Me.cbFilter)
         Me.Controls.Add(Me.btnPrint)
         Me.Controls.Add(Me.ReportData)
         Me.Controls.Add(Me.ReportEnd)
         Me.Controls.Add(Me.ReportStart)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.cbData)
         Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.btnSearchRep)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.cbFilter)
         Me.Controls.Add(Me.timedate)
         Me.Controls.Add(Me.Label3)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -284,15 +297,16 @@ Partial Class HeadReports
     Friend WithEvents timedate As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Timer1 As Timer
-    Friend WithEvents cbFilter As ComboBox
     Friend WithEvents Label4 As Label
     Friend WithEvents btnSearchRep As Guna.UI2.WinForms.Guna2Button
     Friend WithEvents Label8 As Label
     Friend WithEvents Label9 As Label
-    Friend WithEvents cbData As ComboBox
     Friend WithEvents Label1 As Label
     Friend WithEvents ReportEnd As Guna.UI2.WinForms.Guna2DateTimePicker
     Friend WithEvents ReportStart As Guna.UI2.WinForms.Guna2DateTimePicker
     Friend WithEvents ReportData As Guna.UI2.WinForms.Guna2DataGridView
     Friend WithEvents btnPrint As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents cbFilter As ComboBox
+    Friend WithEvents cbData As ComboBox
+    Friend WithEvents btnAttendance As Guna.UI2.WinForms.Guna2Button
 End Class
--- EventProject/HeadReports.vb
+++ EventProject/HeadReports.vb
@@ -19,7 +19,7 @@ Public Class HeadReports
         ' Columns from StudentInformation table
         Dim studentColumns As String() = {"First_Name", "Last_Name", "Department", "Course", "Year", "Section", "StudentID"}
         ' Columns from events table
-        Dim eventColumns As String() = {"venue", "facilitator"}
+        Dim eventColumns As String() = {"venue", "facilitator", "eventname"}
 
         ' Add valid columns to cbFilter
         cbFilter.Items.Clear()
@@ -33,7 +33,7 @@ Public Class HeadReports
 
         Dim sqlQuery As String
         Dim isStudentColumn As Boolean = {"First_Name", "Last_Name", "Department", "Course", "Year", "Section", "StudentID"}.Contains(selectedFilter)
-        Dim isEventColumn As Boolean = {"venue", "facilitator"}.Contains(selectedFilter)
+        Dim isEventColumn As Boolean = {"venue", "facilitator", "eventname"}.Contains(selectedFilter)
 
         If isStudentColumn Then
             sqlQuery = $"SELECT DISTINCT {selectedFilter} FROM StudentInformation"
@@ -91,7 +91,7 @@ Public Class HeadReports
             sqlQuery = $"SELECT * FROM {targetTable} WHERE {filterColumn} = @FilterValue"
 
             ' Check if the filter is from the events table
-        ElseIf filterColumn = "venue" OrElse filterColumn = "facilitator" Then
+        ElseIf filterColumn = "venue" OrElse filterColumn = "facilitator" OrElse filterColumn = "eventname" Then
             targetTable = "events"
             sqlQuery = $"SELECT * FROM {targetTable} WHERE {filterColumn} = @FilterValue"
 
@@ -126,4 +126,91 @@ Public Class HeadReports
         End Try
     End Sub
 
+    Private Sub btnAttendance_Click(sender As Object, e As EventArgs) Handles btnAttendance.Click
+        Dim filterColumn As String = cbFilter.Text
+        Dim filterValue As String = cbData.Text
+
+        If String.IsNullOrEmpty(filterColumn) OrElse String.IsNullOrEmpty(filterValue) Then
+            MessageBox.Show("Please select a filter and a value from the comboboxes.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Initialize variables for the query
+        Dim sqlQuery As String = ""
+        Dim studentColumns As String() = {"First_Name", "Last_Name", "Department", "Course", "Year", "Section", "StudentID"}
+
+        Try
+            ' Build query based on filter type
+            If studentColumns.Contains(filterColumn) Then
+                sqlQuery = "SELECT attendeeid, eventid, eventname, eventdate, StudentID, " &
+                      "First_Name, Last_Name, Department, Course, Year, Section, " &
+                      "timein_time, timeout_time " &
+                      "FROM attendancelog " &
+                      $"WHERE {filterColumn} = @FilterValue " &
+                      "ORDER BY eventdate DESC, timein_time DESC"
+            ElseIf {"venue", "facilitator", "eventname"}.Contains(filterColumn) Then
+                ' For event-related filters, we need to join with events table since these columns are not in attendancelog
+                If filterColumn = "eventname" Then
+                    sqlQuery = "SELECT attendeeid, eventid, eventname, eventdate, StudentID, " &
+                          "First_Name, Last_Name, Department, Course, Year, Section, " &
+                          "timein_time, timeout_time " &
+                          "FROM attendancelog " &
+                          "WHERE eventname = @FilterValue " &
+                          "ORDER BY eventdate DESC, timein_time DESC"
+                Else
+                    sqlQuery = "SELECT al.attendeeid, al.eventid, al.eventname, al.eventdate, " &
+                          "al.StudentID, al.First_Name, al.Last_Name, al.Department, " &
+                          "al.Course, al.Year, al.Section, al.timein_time, al.timeout_time " &
+                          "FROM attendancelog al " &
+                          "INNER JOIN events e ON al.eventid = e.eventid " &
+                          $"WHERE e.{filterColumn} = @FilterValue " &
+                          "ORDER BY al.eventdate DESC, al.timein_time DESC"
+                End If
+            Else
+                MessageBox.Show("Invalid filter selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            ' Execute the query
+            dt = New DataTable()
+            cmd = New MySqlCommand(sqlQuery, conn)
+            cmd.Parameters.AddWithValue("@FilterValue", filterValue)
+            da = New MySqlDataAdapter(cmd)
+
+            If conn.State = ConnectionState.Closed Then conn.Open()
+            da.Fill(dt)
+
+            ' Format datetime columns for better display
+            If dt.Rows.Count > 0 Then
+                If dt.Columns.Contains("timein_time") Then
+                    For Each row As DataRow In dt.Rows
+                        If Not IsDBNull(row("timein_time")) Then
+                            row("timein_time") = Convert.ToDateTime(row("timein_time")).ToString("yyyy-MM-dd HH:mm:ss")
+                        End If
+                    Next
+                End If
+                If dt.Columns.Contains("timeout_time") Then
+                    For Each row As DataRow In dt.Rows
+                        If Not IsDBNull(row("timeout_time")) Then
+                            row("timeout_time") = Convert.ToDateTime(row("timeout_time")).ToString("yyyy-MM-dd HH:mm:ss")
+                        End If
+                    Next
+                End If
+            End If
+
+            ' Display results in the grid
+            ReportData.DataSource = dt
+            ReportData.ColumnHeadersHeight = 30
+            ReportData.AutoResizeColumns()
+
+            If dt.Rows.Count = 0 Then
+                MessageBox.Show("No attendance records found for the selected filter.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show($"Error loading attendance data: {ex.Message}" & vbCrLf & $"SQL Query: {sqlQuery}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
 End Class
